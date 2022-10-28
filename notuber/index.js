12456d32aeab4e329b41aab5afab0910cd7d6eff
sarahfom <sarahrfom@gmail.com>
2022-10-28 10:52:24 -0400

    Lab10: Successful got data back from server

--- notuber/index.js
+++ notuber/index.js
@@ -1,53 +1,74 @@
 function init()
-      {
-        var test = navigator.geolocation.getCurrentPosition(showPosition);
-      	function showPosition(position) 
-      	{
+{
+	var test = navigator.geolocation.getCurrentPosition(showPosition);
+	function showPosition(position) 
+    {
       		var lat = position.coords.latitude;
       		var lng = position.coords.longitude;
       		const landmark = new google.maps.LatLng(lat, lng);
-      		// map.setCenter(new google.maps.LatLng(lat, lng));
-      		var myOptions = {
-          zoom: 13, 
-          center: landmark,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        
-        var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-        var marker = new google.maps.Marker(
-        {
-          position: landmark,
-          title: "Home"
-        });
-        marker.setMap(map);
+      		var myOptions = 
+      		{
+      			zoom: 13, 
+          		center: landmark,
+          		mapTypeId: google.maps.MapTypeId.ROADMAP
+          	};
+          	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+          	var marker = new google.maps.Marker(
+          	{
+          		position: landmark,
+          		title: "Home"
+          	});
+          	marker.setMap(map);
+          	const car = "car.png";
 
-        const car = "car.png";
-        // jsonData = '[{"id":"mXfkjrFw","latitude":"42.3453","longitude":"-71.0464"},{"id":"nZXB8ZHz","latitude":"42.3662","longitude":"-71.0621"},{"id":"Tkwu74WC","latitude":"42.3603","longitude":"-71.0547"},{"id":"5KWpnAJN","latitude":"42.3472","longitude":"-71.0802"},{"id":"uf5ZrXYw","latitude":"42.3663","longitude":"-71.0544"},{"id":"VMerzMH8","latitude":"42.3542","longitude":"-71.0704"}]';
-        // var parsedJsonData = JSON.parse(jsonData);
-        // for (var i = 0; i < parsedJsonData.length; i++) 
-        // {
-        //     var vid = parsedJsonData[i].id;
-        //     var lat = parsedJsonData[i].latitude;
-        //     var lon = parsedJsonData[i].longitude;
-        //     var notUber = new google.maps.LatLng(lat, lon);
-        //     var marker = new google.maps.Marker({
-        //       position: notUber,
-        //       title: vid,
-        //       icon: car
-        //     });
-        //     marker.setMap(map);
-// 
-        // }
+        	// Step 1: make an instance of XHR
+        	request = new XMLHttpRequest();
         
-        var infowindow = new google.maps.InfoWindow();
 
-        
-        google.maps.event.addListener(marker, 'click', function() 
-        {
-          infowindow.setContent(marker.title);
-          infowindow.open(map, marker);
-        });
-  		}
+        	// Step 2: Make request to the JSON source...
+        	request.open('POST', 'https://jordan-marsh.herokuapp.com/rides', true);
+        	console.log("Opened");
+        	request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        	console.log("setRequestHeader");
+        	// Step 2A: Dealing with receiving the HTTP response from XHR
+        	request.onreadystatechange = function() 
+        	{
+        		// This function deals with receiving HTTP response...
+        		console.log("In onreadystatechange");
+        		if (request.readyState == 4 && request.status == 200)  
+        		{
+        			
+        			console.log("Passed readyState");
+        			resultingData = request.responseText; // responseText => string
+					parsedJsonData = JSON.parse(resultingData);
+					console.log(jsonData);
+					// for (var i = 0; i < parsedJsonData.length; i++) 
+			  //       {
+			  //           var vid = parsedJsonData[i].id;
+			  //           var lat = parsedJsonData[i].latitude;
+			  //           var lon = parsedJsonData[i].longitude;
+			  //           var notUber = new google.maps.LatLng(lat, lon);
+			  //           var marker = new google.maps.Marker({
+			  //             position: notUber,
+			  //             title: vid,
+			  //             icon: car
+			  //           });
+			  //           marker.setMap(map);
 
-        
-      }
+			  //       }
+        		}
+        		
+    		}
+    		// Step 3: Send the request...
+    		console.log("Send request");
+    		var username = "uVnnFbz7";
+        	var params = "username="+username+"&lat="+lat+"&lng="+lng
+        	request.send(params);
+            var infowindow = new google.maps.InfoWindow();
+            google.maps.event.addListener(marker, 'click', function() 
+            {
+            	infowindow.setContent(marker.title);
+          		infowindow.open(map, marker);
+          	});
+  	}        
+}
